76b18efa825d72fc0ce8142afebb024dc1aa96dc
Aidan Amavi <mail@aidanamavi.com>
2015-08-06 17:51:22 -0400

    Enhanced meta description function.
    Added meta description process for tag pages.
    Fixed meta description for blog posts.
    Added function to process all types of content for meta description.

--- functions.php
+++ functions.php
@@ -68,14 +68,35 @@ function custom_the_category($separator = '', $parents='', $post_id = false) {
 
 function custom_meta_description() {
 	global $post;
+	function prepare_content($content){
+		$content = ucfirst(trim(strip_tags($content)));
+		// Remove new lines.
+		$content = preg_replace('/\s+/', ' ', $content);
+		// Shortens length. May not be needed. Testing with Google indexing.
+		if (strlen($content) > 4000) {
+			$content = substr($content, 0, 4000).'...';
+		}
+		return $content;
+	}
 	if (is_single()) {
-		$description = get_post_meta( get_the_ID(), 'slide_1_description', true );
+		if ($post->post_type === 'work') {
+			$description = prepare_content(get_post_meta( get_the_ID(), 'slide_1_description', true ));
+		} elseif ($post->post_type === 'blog') {
+			if (have_posts()) {
+				while(have_posts()) {
+					the_post();
+					$description = prepare_content(get_the_content());
+				}
+			}
+		}
 	} elseif (is_home() || is_page()) {
 		$description =  get_bloginfo( 'description' );
 	} elseif (is_category()) {
-		$description =  trim(strip_tags(category_description()));
+		$description =  prepare_content(category_description());
 	} elseif (is_archive()) {
 		$description =  'Archive of all '.$post->post_type.' posts.';
+	} elseif (is_tag()) {
+		$description =  'Archive of all posts with the tag "'.prepare_content(single_tag_title('', FALSE)).'".';
 	}
 	if (empty($description)) {
 		$description =  get_bloginfo( 'description' );
