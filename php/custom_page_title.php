0237bd14e7e0f8b68f25ea5357d4d1f9c5cd3b35
Aidan Amavi <mail@aidanamavi.com>
2020-07-05 17:41:35 -0400

    Fixed archive title display

--- php/custom_page_title.php
+++ php/custom_page_title.php
@@ -21,7 +21,7 @@ function custom_page_title() {
 		$title .= ' &rsaquo; '.ucwords($postType);
 	}
 	//$title .= wp_title('&rsaquo;', false, 'left');
-	if(!is_home() && !is_front_page()){
+	if(!is_home() && !is_front_page() && !is_archive()){
 		$title .= ' &rsaquo; '.get_the_title();
 	}
 	echo $title;
