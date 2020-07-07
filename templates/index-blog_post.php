9c4432d8564e4cd9609ddd98cbfb13e58703eb37
Aidan Amavi <mail@aidanamavi.com>
2020-07-07 19:17:09 -0400

    Updated element attr for controllers

--- templates/index-blog_post.php
+++ templates/index-blog_post.php
@@ -12,7 +12,7 @@
 	* @license https://www.gnu.org/licenses/agpl.html GNU Affero General Public License
 	*/
 ?>
-				<div id="page_single_blog_<?php the_ID(); ?>"  data-page-title="<?php the_title_attribute(); ?>" data-post-type="blog" data-post-id="<?php the_ID(); ?>">
+				<div id="page_single_blog_<?php the_ID(); ?>"  data-page-title="<?php the_title_attribute(); ?>" data-view-type="single" data-post-type="blog" data-post-id="<?php the_ID(); ?>">
 					<div class="title_wrapper">
 						<div class="title">
 							<h2>
@@ -24,7 +24,7 @@
 						<div class="hightlight_number_1">
 							<div class="highlight_text">
 								<article>
-									<h1 class="blog_title"><a href="<?php the_permalink() ?>" data-link-type="postNavigation" data-page="single" data-post-type="blog" data-post-id="<?php the_ID(); ?>"><?php the_title(); ?></a></h1>
+									<h1 class="blog_title"><a href="<?php the_permalink() ?>" data-link-type="postNavigation" data-view-type="single" data-post-type="blog" data-post-id="<?php the_ID(); ?>"><?php the_title(); ?></a></h1>
 									<h4 class="blog_date_categories_tags"><?php the_time('F j, Y'); ?> • <?php custom_the_category(', ',''); ?><?php the_tags(' • '); ?></h4>
 									<div class="blog_content">
 										<?php global $post; setup_postdata($post); the_content(); wp_reset_postdata(); ?>
