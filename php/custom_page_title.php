1e446d719f6db30bebe22c28b6960bd6adf48bd7
Aidan Amavi <mail@aidanamavi.com>
2020-07-18 06:03:44 -0400

    Improved page title support

--- php/custom_page_title.php
+++ php/custom_page_title.php
@@ -8,50 +8,25 @@
 * @license https://www.gnu.org/licenses/agpl.html GNU Affero General Public License
 */
 
-// Add custom page title.
-function custom_page_title() {
-	$siteDescription = get_bloginfo('description');
-	$siteTitle = get_bloginfo('name');
-	$pageTitle = get_the_title();
-	$viewType;
-	$postType = get_post_type();
-	$pageSeperator = ' &rsaquo; ';
-	$newSiteTitle;
-
-	if ( is_front_page() && is_home() ) {
-		$viewType = 'front_page';
-	} elseif (is_singular()){
-		// ‘is_page || is_attachment || is_single’
-		$viewType = 'singular';
-	} elseif (is_archive()){
-		if (is_category()){
-			global $cat;
-			$viewType = 'category';
-			$pageTitle = get_cat_name($cat);
-		} else {
-			$viewType = 'archive';
-		}
-	}
-
-	if($viewType === 'category'){
-		$postType = ucwords($postType);
-		$viewType = ucwords($viewType);
-		$newSiteTitle = $siteTitle.$pageSeperator.$postType.$pageSeperator.$viewType.$pageSeperator.$pageTitle;
-	} elseif ($viewType === 'archive') {
-		$postType = ucwords($postType);
-		$viewType = ucwords($viewType);
-		$newSiteTitle = $siteTitle.$pageSeperator.$postType.$pageSeperator.$viewType;
-	} elseif ($viewType === 'singular'){
-		$postType = ucwords($postType);
-		$newSiteTitle = $siteTitle.$pageSeperator.$postType.$pageSeperator.$pageTitle;
-		// TODO:  Remove hack when about postType is complete; make resume post with about postType
-		if(is_page('about') || is_page('146')){
-			$newSiteTitle = $siteTitle.$pageSeperator.'About'.$pageSeperator.$pageTitle;
-		}
-	} elseif ($viewType === 'front_page'){
-		$newSiteTitle = $siteTitle;
-	}
-	echo $newSiteTitle;
+/*
+ * Let WordPress manage the document title.
+ * By adding theme support, we declare that this theme does not use a
+ * hard-coded <title> tag in the document head, and expect WordPress to
+ * provide it for us.
+ */
+function theme_slug_setup() {
+ add_theme_support( 'title-tag' );
 }
-// Aidan Amavi >  About > Resume
+add_action( 'after_setup_theme', 'theme_slug_setup' );
+
+// Backwards compatibility with older versions of WordPress less than 4.1
+if ( ! function_exists( '_wp_render_title_tag' ) ) :
+  function theme_slug_render_title() {
+		?>
+		<title><?php wp_title( '|', true, 'right' ); ?></title>
+		<?php
+  }
+  add_action( 'wp_head', 'theme_slug_render_title' );
+endif;
+
 ?>
