981150d853c0c7edf0029b322af2f7af5053de3b
Aidan Amavi <mail@aidanamavi.com>
2020-07-18 08:33:55 -0400

    Added WordPress reference URL

--- php/custom_page_title.php
+++ php/custom_page_title.php
@@ -9,11 +9,12 @@
 */
 
 /*
- * Let WordPress manage the document title.
- * By adding theme support, we declare that this theme does not use a
- * hard-coded <title> tag in the document head, and expect WordPress to
- * provide it for us.
- */
+* Let WordPress manage the document title.
+* By adding theme support, we declare that this theme does not use a
+* hard-coded <title> tag in the document head, and expect WordPress to
+* provide it for us.
+* https://codex.wordpress.org/Title_Tag
+*/
 function theme_slug_setup() {
  add_theme_support( 'title-tag' );
 }
