2cb700feb831e9a57710c36ca52d30cd1ec8d68f
Aidan Amavi <mail@aidanamavi.com>
2020-07-19 17:05:31 -0400

    Improved first paint response time

--- header.php
+++ header.php
@@ -11,7 +11,7 @@
 */
 ?>
 <!DOCTYPE html>
-<html xmlns="https://www.w3.org/1999/xhtml" xml:lang="en" lang="en" class="toggleFade">
+<html xmlns="https://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
 	<head>
 		<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
 		<meta name="viewport" content="width=device-width, initial-scale=1.0">
